2a273c9180a34d66402aa78d99f26e4cb0581991
Arthur1asdf <arthurteng0819@gmail.com>
2023-10-07 14:58:45 -0400

    Transactions from statement
    gets only the $$$ from the bank statment

--- main.py
+++ main.py
@@ -1,17 +1,29 @@
-# This is a sample Python script.
-
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
-
-
-print('zamn')
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+# opens file
+statement = open("September2023_2442.csv", "r")
+# print("Name of the file: ", statement.name)
+# takes each line in a file and puts it in a list
+listOfStatements = statement.readlines()
+untilChar = '",'
+allTransactionsString = []
+allTransactionsFloat = []
+counterForArray = 0
+for x in listOfStatements:
+    # gets rid of necessary white space
+    x = x.strip()
+    if untilChar in x:
+        # if the until char is found in the line of the file we split it to the content after the car until char
+        contentAfterStart = x.split(untilChar, -2)[-1]
+        # inserting the split content into a string array
+        allTransactionsString.insert(counterForArray, contentAfterStart)
+        # counter for the array
+        counterForArray += 1
+        # print(allTransactionsString)
+# changes the string to positive and changes it to float and stores it in a float array
+for element in allTransactionsString:
+    # subtracts the first char from the string
+    newString = element[1:]
+    # changes the string to a float so we can store it into an float array later to use
+    newElement = float(newString)
+    allTransactionsFloat.append(newElement)
+print(allTransactionsFloat)
+statement.close()
